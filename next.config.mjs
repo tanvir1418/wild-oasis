1e5cc881c6543b9b29dd72ef480b06994a30b57f
tanvir1418 <tanvirahmed1418@gmail.com>
2025-05-18 19:23:41 +0600

    Project structure, design of pages, dynamic data fetching

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,15 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  images: {
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "ogxmpzpmnrksvxgjicpj.supabase.co",
+        pathname: "/storage/v1/object/public/cabin-images/**",
+      },
+    ],
+  },
+  // output: "export",
+};
 
 export default nextConfig;
